a87bd421ef3b715354526e4b7e4af8bd7b9dd978
robinbenitezmora <robinbenitez1990@gmail.com>
2022-10-25 13:41:02 -0500

    Add code lines in 'schema' file in order to modify and create tables

--- schema.sql
+++ schema.sql
@@ -12,3 +12,35 @@ CREATE TABLE animals (
 
 /*Add a column species of type string to your animals table*/
 ALTER TABLE animals ADD COLUMN species VARCHAR(50);
+
+/*Insert the following data:*/
+-- Create a table named owners with the following columns:
+-- id: integer (set it as autoincremented PRIMARY KEY)
+-- full_name: string
+-- age: integer
+CREATE TABLE owners(
+  id   int GENERATED ALWAYS AS IDENTITY,
+  full_name  varchar,
+  age  int,
+  PRIMARY KEY (id)
+);
+
+-- Create a table named species with the following columns:
+-- id: integer (set it as autoincremented PRIMARY KEY)
+-- name: string
+CREATE TABLE species(
+  id   int GENERATED ALWAYS AS IDENTITY,
+  name  varchar,
+   PRIMARY KEY (id)	
+);
+
+-- Remove column species
+ALTER TABLE animals DROP COLUMN species;
+
+-- Add column species_id which is a foreign key referencing species table
+ALTER TABLE animals  ADD COLUMN species_id INT;
+ALTER TABLE animals ADD FOREIGN KEY (species_id)  REFERENCES species(id); 
+
+-- Add column owner_id which is a foreign key referencing the owners table
+ALTER TABLE animals  ADD COLUMN owner_id INT;
+ALTER TABLE animals ADD FOREIGN KEY (owner_id)  REFERENCES owners(id); 
