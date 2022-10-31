eb052dbb7e67e142c6f69e67e3dda19643b1fef7
robinbenitezmora <robinbenitez1990@gmail.com>
2022-10-31 07:25:30 -0500

    Modify code lines in 'data.sql'

--- data.sql
+++ data.sql
@@ -133,7 +133,13 @@ VALUES ('2020-05-24', 1, 1), ('2020-07-22', 1, 3), ('2021-02-02', 2, 4),
 
 	-- This will add 3.594.220 visits considering you have 10 animals and 4 vets, 
 	-- and it will use around 87.000 timestamps (4 minutes aprox).
-	INSERT INTO visits (animals_id, vets_id, date_of_visit) SELECT * FROM (SELECT id FROM animals) animals_id, (SELECT id FROM vets) vets_id, generate_series('1980-01-01'::timestamp, '2021-01-01'::timestamp, '4 hours') visit_timestamp;
+	INSERT INTO visits (animals_id, vets_id, date_of_visit) 
+	SELECT * FROM 
+	(SELECT id FROM animals) animals_id, 
+	(SELECT id FROM vets) vets_id, generate_series('1980-01-01'::timestamp, '2021-01-01'::timestamp, '4 hours') visit_timestamp;
 
 	-- This will add 2.500.000 owners with full_name = 'Owner <X>' and email = 'owner_<X>@email.com' (2 minutes aprox).
-	INSERT INTO owners (full_name, email) SELECT 'Owner ' || generate_series(1, 2500000), 'owner_' || generate_series(1, 2500000) || '@email.com';
+	INSERT INTO owners (full_name, email) 
+	SELECT 'Owner ' || generate_series(1, 2500000), 'owner_' 
+	|| generate_series(1, 2500000) || '@mail.com';
+	
