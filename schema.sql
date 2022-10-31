2b36bcae062a5347b889cc6e6b16b1e9a5bdd047
robinbenitezmora <robinbenitez1990@gmail.com>
2022-10-31 07:02:20 -0500

    Add email column to the 'owners' table

--- schema.sql
+++ schema.sql
@@ -86,3 +86,6 @@ CREATE TABLE visits (
     FOREIGN KEY (vets_id) REFERENCES vets(id),
     PRIMARY KEY (id)
 );
+
+--Add an email column to owners table
+ALTER TABLE owners ADD COLUMN email VARCHAR(120);
