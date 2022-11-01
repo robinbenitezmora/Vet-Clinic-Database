292eafff216caf5fb632aa14488924b11756c3ad
robinbenitezmora <robinbenitez1990@gmail.com>
2022-11-01 10:18:06 -0500

    Add the foreign keys in order to connect the tables data

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -1,24 +1,26 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE patients (
-    id integer GENERATED ALWAYS AS IDENTITY,
-    name character varying(50) NOT NULL,
+    id INT GENERATED ALWAYS AS IDENTITY,
+    name character VARCHAR(50) NOT NULL,
     date_of_birth date NOT NULL,
     PRIMARY KEY (id)
 );
 
-CREATE TABLE medical_histories(
-  id   int GENERATED ALWAYS AS IDENTITY,
-  admitted_at  timestamp,
-  patient_id  int,
-  status  varchar,
+CREATE TABLE medical_histories (
+  id INT GENERATED ALWAYS AS IDENTITY,
+  admitted_at TIMESTAMP NOT NULL,
+  patient_id INT NOT NULL,
+  status  VARCHAR(50) NOT NULL,
+  FOREIGN KEY (patient_id) REFERENCES patients(id),
+  FOREIGN KEY (id) REFERENCES treatments(id),
   PRIMARY KEY (id)
 );
 
-CREATE TABLE treatments(
-  id   int GENERATED ALWAYS AS IDENTITY,
-  type  varchar,
-  name  varchar,
+CREATE TABLE treatments (
+  id INT GENERATED ALWAYS AS IDENTITY,
+  type VARCHAR(50),
+  name VARCHAR(50),
   PRIMARY KEY (id)
 );
 
@@ -28,6 +30,7 @@ CREATE TABLE invoices (
   generated_at TIMESTAMP,
   paid_at TIMESTAMP,
   medical_history_id INT,
+  FOREIGN KEY (medical_history_id) REFERENCES medical_histories(id),
   PRIMARY KEY (id)
 );
 
@@ -38,5 +41,16 @@ CREATE TABLE invoice_items (
     total_price DECIMAL(10,2),
     invoice_id INT,
     treatment_id INT,
+    FOREIGN KEY (invoice_id) REFERENCES invoices(id),
+    FOREIGN KEY (treatment_id) REFERENCES treatments(id),
     PRIMARY KEY (id)
+);
+
+CREATE TABLE medical_histories_treatments (
+  id INT GENERATED ALWAYS AS IDENTITY,
+  medical_history_id INT,
+  treatment_id INT,
+  FOREIGN KEY (medical_history_id) REFERENCES medical_histories(id),
+  FOREIGN KEY (treatment_id) REFERENCES treatments(id),
+  PRIMARY KEY (id)
 );
