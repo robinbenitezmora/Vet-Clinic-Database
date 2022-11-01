54e7f94c69f1e94e5d4c9b8a5ec68deb358d8661
robinbenitezmora <robinbenitez1990@gmail.com>
2022-11-01 09:09:58 -0500

    Add code lines for a new 'invoice_items' table

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -31,32 +31,12 @@ CREATE TABLE invoices (
   PRIMARY KEY (id)
 );
 
-CREATE TABLE specialization (
+CREATE TABLE invoice_items (
     id INT GENERATED ALWAYS AS IDENTITY,
-    vet_id INT,
-    species_id INT,
-    FOREIGN KEY (vet_id) REFERENCES vets(id),
-    FOREIGN KEY (species_id) REFERENCES  species(id),
+    unit_price DECIMAL(10,2),
+    quantity INT,
+    total_price DECIMAL(10,2),
+    invoice_id INT,
+    treatment_id INT,
     PRIMARY KEY (id)
 );
-
--- There is a many-to-many relationship between the tables animals and vets: 
--- an animal can visit multiple vets and one vet can be visited by multiple animals. 
--- Create a "join table" called visits to handle this relationship
--- it should also keep track of the date of the visit.
-CREATE TABLE visits (
-    id INT GENERATED ALWAYS AS IDENTITY,
-    animals_id INT,
-    vets_id INT,   	
-    date_of_visit VARCHAR(50),
-    FOREIGN KEY (animals_id) REFERENCES  animals(id),
-    FOREIGN KEY (vets_id) REFERENCES vets(id),
-    PRIMARY KEY (id)
-);
-
---Add an email column to owners table
-ALTER TABLE owners ADD COLUMN email VARCHAR(120);
-
-CREATE INDEX index_animals_vets_visits ON visits (animals_id, vets_id);
-
-CREATE INDEX index_email_owners ON owners (email);
